c06785cda05d14ad1322fcf836e695a8c2387bd4
Maria Ivanova <ivanova.m241@gmail.com>
2020-02-03 10:10:42 +0300

    first commit

---
+++ 01_types.js
@@ -0,0 +1,26 @@
+// null, undefined, boolean, number, string, object, symbol
+
+// console.log(typeof undefined) // переменная не объявлена
+// console.log(typeof 0)
+// console.log(typeof true)
+// console.log(typeof 'Javascript')
+// console.log(typeof Math)
+// console.log(typeof Symbol('js'))
+// console.log(typeof null) // переменная объявлена, но в ней нет значения
+// console.log(typeof NaN)
+
+// Приведение типов
+// const language = 'JavaScript'
+
+// if (language) {
+//   console.log('The best language is: ', language)
+// }
+
+// '', 0, null, undefined, NaN, false => false
+// [], {}, function(){} => true
+// console.log(Boolean({}))
+
+// Строки и числа
+console.log(1 + '2') // string 12
+console.log('' + 1 + 0) // string 10
+console.log('' - 1 + 0) // number -1
